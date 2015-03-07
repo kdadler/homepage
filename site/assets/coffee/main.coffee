940f43dc116c80122432a9d0a02fac707b7be235
Kieran Adler <kdadler@gmail.com>
2015-03-07 20:08:53 +0000

    Nearing completion of desktop site

--- site/assets/coffee/main.coffee
+++ site/assets/coffee/main.coffee
@@ -1,20 +1,118 @@
 $ = jQuery
 
-# Header controllers.
-reduced = false
-minHead = ->
-  $('#header').addClass 'reduced'
-  reduced = true
-maxHead = ->
-  $('#header').removeClass 'reduced'
-  reduced = false
-updateHead = ->
-  scroll = $(window).scrollTop()
-  if reduced and scroll < 31 then maxHead()
-  else if !reduced and scroll > 30 then minHead()
+class pageController
+  reduced       : false
+  locations     : {}
+  currentLocale : 'title'
+  images        : [
+    '/images/gallery/DSCF6547.jpg'
+    '/images/gallery/IMG_0042.jpg'
+    '/images/gallery/DSC_0102.JPG'
+    '/images/gallery/DSC_0173.JPG'
+    '/images/gallery/DSC_0275.JPG'
+    '/images/gallery/DSCF1496.JPG'
+    '/images/gallery/P1140056.JPG'
+    '/images/gallery/P6290102.JPG'
+    '/images/gallery/P7010155.JPG'
+    '/images/gallery/P7030182.JPG'
+  ]
 
 
+  # Page initialise.
+  init: ->
+    @addImages()
+    @events()
+    @indexLocations()
 
+  events: ->
+    _t = @
+    $(window).scroll (e) =>
+      @updateHead()
+      @updateLocale()
+    $('.image').click ->
+      _t.initGallery parseInt $(@).attr 'index'
+    $('.navigate').click (e) ->
+      e.preventDefault()
+      _t.navigateTo @
+    $('#header-logo').click =>
+      @scrollTo 0
+
+
+  # Header controllers.
+  minHead: ->
+    $('#header').addClass 'reduced'
+    @reduced = true
+
+  maxHead: ->
+    $('#header').removeClass 'reduced'
+    @reduced = false
+
+  updateHead: ->
+    scroll = $(window).scrollTop()
+    if @reduced and scroll < 31 then @maxHead()
+    else if !@reduced and scroll > 30 then @minHead()
+
+
+  # Scroll to nonsense.
+  getName: (element) ->
+    id = $(element).attr('id').split '-'
+    id[0]
+
+  navigateTo: (element) ->
+    name = @getName element
+    @scrollTo @locations[name]
+
+  scrollTo: (position) ->
+    $('html, body').animate
+        scrollTop: position
+      , 1000
+
+  indexLocations: ->
+    _t = @
+    $('.section').each ->
+      name               = _t.getName @
+      position           = $("##{name}-section").offset().top - 40
+      _t.locations[name] = Math.max position, 0
+
+  updateLocale: ->
+    locale = @getLocale()
+    if locale isnt @currentLocale
+      @currentLocale = locale
+      $('.current').removeClass 'current'
+      $("##{locale}-link").addClass 'current'
+
+  getLocale: ->
+    scroll   = $(window).scrollTop()
+    previous = 0
+    for name, position of @locations
+      if (position - 60) < scroll
+        previous = name
+      else
+        return previous
+    name
+
+
+  # Image gallery functionality.
+  initGallery: (index) ->
+    element = document.querySelectorAll '.pswp'
+    items   = @getImageItems()
+    options =
+      index: index
+    gallery = new PhotoSwipe element[0], PhotoSwipeUI_Default, items, options
+    gallery.init()
+
+  getImageItems: ->
+    for image in @images
+      src : image
+      w   : 900
+      h   : 675
+
+  addImages: ->
+    for image, i in @images
+      $('#gallery').append "<img class=\"image\" src=\"#{image}\" index=\"#{i}\">"
+
+
+# Page initialisation.
 $ ->
-  $(window).scroll (e) ->
-    updateHead()
+  controller = new pageController()
+  controller.init()
--- site/public/scripts/main.js
+++ site/public/scripts/main.js
@@ -1,34 +1,165 @@
 (function() {
-  var $, maxHead, minHead, reduced, updateHead;
+  var $, pageController;
 
   $ = jQuery;
 
-  reduced = false;
+  pageController = (function() {
+    function pageController() {}
 
-  minHead = function() {
-    $('#header').addClass('reduced');
-    return reduced = true;
-  };
+    pageController.prototype.reduced = false;
 
-  maxHead = function() {
-    $('#header').removeClass('reduced');
-    return reduced = false;
-  };
+    pageController.prototype.locations = {};
 
-  updateHead = function() {
-    var scroll;
-    scroll = $(window).scrollTop();
-    if (reduced && scroll < 31) {
-      return maxHead();
-    } else if (!reduced && scroll > 30) {
-      return minHead();
-    }
-  };
+    pageController.prototype.currentLocale = 'title';
+
+    pageController.prototype.images = ['/images/gallery/DSCF6547.jpg', '/images/gallery/IMG_0042.jpg', '/images/gallery/DSC_0102.JPG', '/images/gallery/DSC_0173.JPG', '/images/gallery/DSC_0275.JPG', '/images/gallery/DSCF1496.JPG', '/images/gallery/P1140056.JPG', '/images/gallery/P6290102.JPG', '/images/gallery/P7010155.JPG', '/images/gallery/P7030182.JPG'];
+
+    pageController.prototype.init = function() {
+      this.addImages();
+      this.events();
+      return this.indexLocations();
+    };
+
+    pageController.prototype.events = function() {
+      var _t,
+        _this = this;
+      _t = this;
+      $(window).scroll(function(e) {
+        _this.updateHead();
+        return _this.updateLocale();
+      });
+      $('.image').click(function() {
+        return _t.initGallery(parseInt($(this).attr('index')));
+      });
+      $('.navigate').click(function(e) {
+        e.preventDefault();
+        return _t.navigateTo(this);
+      });
+      return $('#header-logo').click(function() {
+        return _this.scrollTo(0);
+      });
+    };
+
+    pageController.prototype.minHead = function() {
+      $('#header').addClass('reduced');
+      return this.reduced = true;
+    };
+
+    pageController.prototype.maxHead = function() {
+      $('#header').removeClass('reduced');
+      return this.reduced = false;
+    };
+
+    pageController.prototype.updateHead = function() {
+      var scroll;
+      scroll = $(window).scrollTop();
+      if (this.reduced && scroll < 31) {
+        return this.maxHead();
+      } else if (!this.reduced && scroll > 30) {
+        return this.minHead();
+      }
+    };
+
+    pageController.prototype.getName = function(element) {
+      var id;
+      id = $(element).attr('id').split('-');
+      return id[0];
+    };
+
+    pageController.prototype.navigateTo = function(element) {
+      var name;
+      name = this.getName(element);
+      return this.scrollTo(this.locations[name]);
+    };
+
+    pageController.prototype.scrollTo = function(position) {
+      return $('html, body').animate({
+        scrollTop: position
+      }, 1000);
+    };
+
+    pageController.prototype.indexLocations = function() {
+      var _t;
+      _t = this;
+      return $('.section').each(function() {
+        var name, position;
+        name = _t.getName(this);
+        position = $("#" + name + "-section").offset().top - 40;
+        return _t.locations[name] = Math.max(position, 0);
+      });
+    };
+
+    pageController.prototype.updateLocale = function() {
+      var locale;
+      locale = this.getLocale();
+      if (locale !== this.currentLocale) {
+        this.currentLocale = locale;
+        $('.current').removeClass('current');
+        return $("#" + locale + "-link").addClass('current');
+      }
+    };
+
+    pageController.prototype.getLocale = function() {
+      var name, position, previous, scroll, _ref;
+      scroll = $(window).scrollTop();
+      previous = 0;
+      _ref = this.locations;
+      for (name in _ref) {
+        position = _ref[name];
+        if ((position - 60) < scroll) {
+          previous = name;
+        } else {
+          return previous;
+        }
+      }
+      return name;
+    };
+
+    pageController.prototype.initGallery = function(index) {
+      var element, gallery, items, options;
+      element = document.querySelectorAll('.pswp');
+      items = this.getImageItems();
+      options = {
+        index: index
+      };
+      gallery = new PhotoSwipe(element[0], PhotoSwipeUI_Default, items, options);
+      return gallery.init();
+    };
+
+    pageController.prototype.getImageItems = function() {
+      var image, _i, _len, _ref, _results;
+      _ref = this.images;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        image = _ref[_i];
+        _results.push({
+          src: image,
+          w: 900,
+          h: 675
+        });
+      }
+      return _results;
+    };
+
+    pageController.prototype.addImages = function() {
+      var i, image, _i, _len, _ref, _results;
+      _ref = this.images;
+      _results = [];
+      for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+        image = _ref[i];
+        _results.push($('#gallery').append("<img class=\"image\" src=\"" + image + "\" index=\"" + i + "\">"));
+      }
+      return _results;
+    };
+
+    return pageController;
+
+  })();
 
   $(function() {
-    return $(window).scroll(function(e) {
-      return updateHead();
-    });
+    var controller;
+    controller = new pageController();
+    return controller.init();
   });
 
 }).call(this);
